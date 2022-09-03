76f9b9ee1a1e13975740ba44b6c4a288729849dd
wsydn <wsydney@web.de>
2022-09-03 11:22:14 +0200

    Fix

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,7 @@ use craft\helpers\App;
 
 define('CRAFT_BASE_PATH', __DIR__);
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
+define('CRAFT_TEMPLATES_PATH', CRAFT_BASE_PATH . '/templates');
 
 // Load Composer's autoloader
 require_once CRAFT_VENDOR_PATH . '/autoload.php';
